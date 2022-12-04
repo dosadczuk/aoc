6914077238db182ebf97cd9fb95290f63bac74e3
dosadczuk <damian.osadczuk@gmail.com>
2022-12-04 12:55:19 +0100

    AOC 2022: change set impl

--- 2022/go.mod
+++ 2022/go.mod
@@ -3,3 +3,5 @@ module github.com/dosadczuk/aoc/2022
 go 1.19
 
 require golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9
+
+require github.com/dosadczuk/go-ds v0.0.0-20221204114900-c1535606a64a
--- 2022/pkg/day3/solve.go
+++ 2022/pkg/day3/solve.go
@@ -3,7 +3,7 @@ package day3
 import (
 	"unicode"
 
-	"github.com/dosadczuk/aoc/2022/internal/ds"
+	"github.com/dosadczuk/go-ds/ds"
 )
 
 func Solve1(input []string) int {
@@ -12,7 +12,7 @@ func Solve1(input []string) int {
 	for _, entry := range input {
 		var half int = len(entry) / 2
 
-		chars := ds.SetIntersection(
+		chars := ds.HashSetIntersection(
 			getUniqueChars(entry[:half]), // left half
 			getUniqueChars(entry[half:]), // right half
 		)
@@ -26,25 +26,25 @@ func Solve1(input []string) int {
 func Solve2(input []string) int {
 	total := 0
 
-	chars := ds.NewSet[rune]()
+	chars := ds.NewHashSet[rune]()
 	for i, entry := range input {
 		if chars.Empty() { // is new group
 			chars = getUniqueChars(entry)
 		} else {
-			chars = ds.SetIntersection(getUniqueChars(entry), chars)
+			chars = ds.HashSetIntersection(getUniqueChars(entry), chars)
 		}
 
 		if ord := i + 1; ord%3 == 0 || ord == len(input) {
 			total += getCharPriority(chars)
-			chars = ds.NewSet[rune]()
+			chars = ds.NewHashSet[rune]()
 		}
 	}
 
 	return total
 }
 
-func getUniqueChars(v string) *ds.Set[rune] {
-	chars := ds.NewSet[rune]()
+func getUniqueChars(v string) *ds.HashSet[rune] {
+	chars := ds.NewHashSet[rune]()
 	for _, char := range v {
 		chars.Add(char)
 	}
@@ -52,7 +52,7 @@ func getUniqueChars(v string) *ds.Set[rune] {
 	return chars
 }
 
-func getCharPriority(s *ds.Set[rune]) int {
+func getCharPriority(s *ds.HashSet[rune]) int {
 	for _, v := range s.Vals() { // first value only
 		if unicode.IsLower(v) {
 			return int(v - 'a' + 01) // priority 01 through 26
